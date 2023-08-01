5fbb8f74dbf34544b5ed2db150e8d3d7fe6bbb63
KelvinPadilla <kelvinpadilla93@gmail.com>
2023-07-31 20:50:33 -0500

    acomode las rutas de las imagenes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const publicPath = path.resolve(__dirname, './public')
 const express = require('express')
 const app = express();
 const PORT = 3030
+
+app.use(express.static(publicPath))
+
 app.get("/", function(req , res){
-    res.sendFile(path.join(__dirname, "views/home.html"))
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
 })
 
 app.listen(PORT, ()=>{
