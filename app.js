52d0ad9bd524f351abe76c76bf17da85345bc0eb
KelvinPadilla <kelvinpadilla93@gmail.com>
2023-08-21 18:47:09 -0500

    Proyecto Terminado

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const path = require('path')
 const publicPath = path.resolve(__dirname, './public')
 const express = require('express')
 const app = express();
-const PORT = 3030
+const PORT = 3001
 
 app.use(express.static(publicPath))
 
 app.get("/", function(req , res){
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 })
+app.get("/register", function(req , res){
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+app.get("/login", function(req , res){
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
 
 app.listen(PORT, ()=>{
     console.log(`server andando en el puerto ${PORT}`)
 }
-
 )
